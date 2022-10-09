45dc7537b4399e77861d5d8a3e308a1e8c047f87
aribl <ariblochorev16@gmail.com>
2022-10-09 10:04:32 +0300

    change name to noam

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi,ari')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi,noam')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
